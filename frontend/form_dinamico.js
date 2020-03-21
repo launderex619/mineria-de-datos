2e95036b8be1cf6aa0b4bbc901e1369c3a3e53fc
Eduardo-Guerra-Alvarez <eduardo.guerraa@alumnos.udg.mx>
2020-03-21 16:51:48 -0600

    Mejora de interfaz

--- frontend/form_dinamico.js
+++ frontend/form_dinamico.js
@@ -1,7 +1,32 @@
+var div = document.createElement("div");
+div.setAttribute("class", "row");
+div.setAttribute("class", "mb-3");
+
+var label = document.createElement("label");
+var texto = document.createTextNode("Etiqueta");
+
+label.appendChild(texto);
+
+div.appendChild(label);
+
+var elemento = document.createElement("input");
+elemento.setAttribute("type", "number");
+elemento.setAttribute("class", "form-control");
+
+div.appendChild(elemento);
+
+document.getElementById("prueba").appendChild(div);
+
+var div = document.createElement("div");
+div.setAttribute("class", "row");
+div.setAttribute("class", "mb-3");
+
+
 var elemento = document.createElement("input");
 
 elemento.setAttribute("type", "number");
 elemento.setAttribute("class", "form-control");
 
-document.getElementById("prueba").appendChild(elemento);
-console.log(elemento);
+div.appendChild(elemento);
+
+document.getElementById("prueba").appendChild(div);
