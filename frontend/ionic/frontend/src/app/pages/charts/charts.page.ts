066a4827b8d5e0266e023fbc33190b51fff09761
Carlos Carvajal Vázquez <carlos12150@hotmail.com>
2020-03-24 18:21:18 -0600

    Charts

--- frontend/ionic/frontend/src/app/pages/charts/charts.page.ts
+++ frontend/ionic/frontend/src/app/pages/charts/charts.page.ts
@@ -5,18 +5,17 @@ import { DataService } from 'src/app/services/data.service';
 import { ModalController, ToastController, AlertController } from '@ionic/angular';
 import { FileService } from 'src/app/services/file.service';
 import * as _ from 'lodash';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-charts',
   templateUrl: './charts.page.html',
   styleUrls: ['./charts.page.scss']
 })
-
 export class ChartsPage implements OnInit {
-
   dataset = null;
   nameDataset = '';
-  columns = [];
+  stadistics = [];
   properties = null;
 
   constructor(
@@ -33,8 +32,7 @@ export class ChartsPage implements OnInit {
     this.nameDataset = version;
   }
 
-   ngOnInit() {
-  }
+  ngOnInit() {}
 
   async ionViewDidEnter() {
     const version = this.route.snapshot.paramMap.get('version');
@@ -51,12 +49,6 @@ export class ChartsPage implements OnInit {
       if (resp.status === 'ok') {
         // @ts-ignore
         this.properties = resp.archivo;
-        if (!this.properties.atributos_archivo_creado._id) {
-          this.columns.push({ name: '_id', prop: '_id' });
-        }
-        this.properties.atributos_archivo_creado.forEach(element => {
-          this.columns.push({ name: element.nombre_atributo, prop: element.nombre_atributo });
-        });
       }
     } catch (error) {
       console.error(error);
@@ -79,18 +71,19 @@ export class ChartsPage implements OnInit {
         // @ts-ignore
         this.dataset = resp.datos;
         const datasetCopy = [];
+        // tslint:disable-next-line: no-shadowed-variable
         this.dataset.forEach((element, index) => {
           element._id = index;
         });
         if (this.dataset.length > 500) {
-          this.presentToast('Por temas de performance, solo se mostraran los primeros 500 elementos, numero de elementos: ' +
-            this.dataset.length);
+          this.presentToast(
+            'Por temas de performance, solo se mostraran los primeros 500 elementos, numero de elementos: ' + this.dataset.length
+          );
           for (let i = 0; i < 500; i++) {
             datasetCopy.push(this.dataset[i]);
           }
           this.dataset = datasetCopy;
         }
-        console.log(this.dataset);
       }
     } catch (error) {
       console.error(error);
@@ -99,18 +92,29 @@ export class ChartsPage implements OnInit {
 
   loadCharts() {
     this.properties.atributos_archivo_creado.forEach(item => {
+      // tslint:disable-next-line: no-shadowed-variable
       const element = document.getElementById(item.nombre_atributo);
       // @ts-ignore
-      Plotly.plot( element, this.getChart(item.tipo_de_dato, item.nombre_atributo));
-      });
+      Plotly.plot(element, this.getChart(item.tipo_de_dato, item.nombre_atributo, item.expresion_regular));
+    });
   }
 
-  getChart( dataType, attribName ) {
+  getChart(dataType, attribName, regex) {
     const data = [];
+    // tslint:disable-next-line: no-shadowed-variable
     this.dataset.forEach(element => {
       data.push(element[attribName]);
     });
-
+    const variables = {
+      missing_instances_number: this.getMissingInstancesNumber(data),
+      missing_instances_list: this.getMissingInstancesString(data),
+      instances_out_domain_number: this.getInstancesOutDomain(data, regex),
+      instances_out_domain_list: this.getInstancesOutDomainString(data, regex),
+      average: this.getAverage(data, dataType),
+      median: this.getMedian(data, dataType),
+      mode: this.getMode(data, dataType)
+    };
+    this.stadistics.push(variables);
     const chart = [];
     if (dataType === 'Numerico') {
       chart.push({
@@ -125,7 +129,7 @@ export class ChartsPage implements OnInit {
       chart.push({
         y: data,
         name: attribName,
-        type: 'histogram',
+        type: 'histogram'
       });
     } else {
       chart.push({
@@ -138,5 +142,112 @@ export class ChartsPage implements OnInit {
       });
     }
     return chart;
+  }
+  getMissingInstancesNumber(data: any[]) {
+    let counter = 0;
+    // tslint:disable-next-line: no-shadowed-variable
+    data.forEach(element => {
+      if (element === this.properties.valor_nulo || element === '') {
+        counter++;
+      }
+    });
+    return counter;
+  }
+  getMissingInstancesString(data: any[]) {
+    let list = '';
+    // tslint:disable-next-line: no-shadowed-variable
+    data.forEach((element, index) => {
+      if (element === this.properties.valor_nulo || element === '') {
+        list += `Elemento faltante: ${index} <br>`;
+      }
+    });
+    return list;
+  }
+  getInstancesOutDomain(data: any[], regex) {
+    let counter = 0;
+    const regExp = new RegExp(regex);
+    // tslint:disable-next-line: no-shadowed-variable
+    data.forEach((element: string, index) => {
+      if (!regExp.test(element)) {
+        counter++;
+      }
+    });
+    return counter;
+  }
+  getInstancesOutDomainString(data: any[], regex) {
+    let list = '';
+    const regExp = new RegExp(regex);
+    // tslint:disable-next-line: no-shadowed-variable
+    data.forEach((element: string, index) => {
+      if (!regExp.test(element)) {
+        list += `Elemento faltante: ${index} <br>`;
+      }
+    });
+    return list;
+  }
+  getAverage(data: any[], dataType) {
+    if (dataType !== 'Numerico') {
+      return null;
+    }
+    let counter = 0;
+    let sum = 0;
+    // tslint:disable-next-line: no-shadowed-variable
+    data.forEach(element => {
+      if (element !== this.properties.valor_nulo && element !== '') {
+        counter++;
+        try {
+          sum += +element;
+        } catch (error) {
+          this.presentToast('Error al intentar castear de cadena a Numero');
+          return null;
+        }
+      }
+    });
+    if (counter === 0) {
+      return 0;
+    }
+    return sum / counter;
+  }
+  getMedian(data: any[], dataType) {
+    if (dataType !== 'Numerico') {
+      return null;
+    }
+    data = data.filter(e => {
+      return e !== '' && e !== this.properties.valor_nulo;
+    });
+    try {
+      data.sort((a, b) => +a - +b);
+    } catch (error) {
+      this.presentToast('Error al intentar castear de cadena a Numero');
+      return null;
+    }
+    const lowMiddle = Math.floor((data.length - 1) / 2);
+    const highMiddle = Math.ceil((data.length - 1) / 2);
+    return (+data[lowMiddle] + +data[highMiddle]) / 2;
+  }
+  getMode(data: any[], dataType) {
+    if (dataType !== 'Numerico') {
+      return null;
+    }
+    data = data.filter(e => {
+      return e !== '' && e !== this.properties.valor_nulo;
+    });
+    return data.sort((a, b) => data.filter(v => v === a).length - data.filter(v => v === b).length).pop();
+  }
+  getPearson(data: any[], dataType: any) {
+    // pendiente
+  }
+  getChiSquare(data: any[], dataType: any) {
+    // pendiente
+  }
+
+  async showList(message) {
+    const alert = await this.alertController.create({
+      header: 'Lista',
+      message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 }
