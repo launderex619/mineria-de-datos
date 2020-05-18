73d2c0445c8462ce3e5c818dd46fef00a48b3185
Israel F <jisraelfc@hotmail.com>
2020-05-17 22:55:20 -0500

    Actualizo normalizacion con opciones

--- frontend/ionic/frontend/src/app/pages/analisis/normalizacion/normalizacion.page.ts
+++ frontend/ionic/frontend/src/app/pages/analisis/normalizacion/normalizacion.page.ts
@@ -48,6 +48,11 @@ export class NormalizacionPage implements OnInit {
   }
 
   normalizarDatos() {
+    var opcion;
+    do {
+     opcion = +prompt("Eliga el metodo de normalizacion. 1 para Min-Max. 2 para Z-score (absoluta). 3 para Z-score (estandar)")
+    }while(opcion < 1 || opcion > 3)
+    
     const max = {};
     const min = {};
     const llavesSinFiltrar = Object.keys(this.settings.settings);
@@ -57,7 +62,8 @@ export class NormalizacionPage implements OnInit {
         llaves.push(llave);
       }
     });
-    
+    //MIN-MAX
+    if(opcion == 1){
     llaves.forEach(llave => {
       max[llave] = Number.MIN_VALUE;
       min[llave] = Number.MAX_VALUE;      
@@ -78,40 +84,38 @@ export class NormalizacionPage implements OnInit {
         elemento[llave] = +((+elemento[llave] - min[llave]) / (max[llave] - min[llave])) * (1 - 0) + 0;
       });
     });
-
-
-    //Para normalizacion Z-square   **********************
-   // console.log("Zscore")    
-    const media = {};    
+  }
+  const media = {};
+  const nDelAtributo = {};
+  llaves.forEach(llave => {
+    if(llave != "sl_no"){
+    media[llave] = 0;
+    nDelAtributo[llave] = 0;
+    }
+  })
+  //primer paso: obtenemos la media de cada atributo
+  this.settings.dataset.forEach(elemento => {
+    llaves.forEach(llave => {
+      if (+elemento[llave] != null && +elemento[llave] != NaN && llave != "sl_no") {
+        media[llave] += +elemento[llave];
+        nDelAtributo[llave]++;
+      }        
+    });
+  });
+  llaves.forEach(llave => {
+    if(llave != "sl_no")
+    media[llave] /= nDelAtributo[llave];
+  });
+  //DESVIACION ESTANDAR ABSOLUTA
+  if(opcion == 2){
     const desviacionEstAbs = {};
-    const desviacionEst = {};
-    const nDelAtributo = {};
-    let n = 0;
     llaves.forEach(llave => {
-      if(llave != "sl_no"){
-      media[llave] = 0;
+      if(llave != "sl_no"){      
       desviacionEstAbs[llave] = 0;
-      desviacionEst[llave] = 0;
-      nDelAtributo[llave] = 0;
       }
-    });
-    //primer paso: obtenemos la media de cada atributo
-    this.settings.dataset.forEach(elemento => {
-      llaves.forEach(llave => {
-        if (+elemento[llave] != null && +elemento[llave] != NaN && llave != "sl_no") {
-          media[llave] += +elemento[llave];
-          nDelAtributo[llave]++;
-        }        
-      });
-    });
-    llaves.forEach(llave => {
-      if(llave != "sl_no")
-      media[llave] /= nDelAtributo[llave];
-    });
-    
-    //Si queremos la tecnica con la desviacion estandar absoluta *************************************
+    });    
     //segunda parte A: encontrar la desviacion estandar absoluta
-    /*this.settings.dataset.forEach(elemento => {
+    this.settings.dataset.forEach(elemento => {
       llaves.forEach(llave => {
         if (+elemento[llave] != null && +elemento[llave] != NaN && llave != "sl_no") {
           desviacionEstAbs[llave] += Math.abs(+elemento[llave] - media[llave])          
@@ -128,9 +132,16 @@ export class NormalizacionPage implements OnInit {
         if (llave != "sl_no" )
         elemento[llave] = +((+elemento[llave] - media[llave]) / desviacionEstAbs[llave] );
       });
-    });*/
-
-    //si queremos la tecnica con la desviacion estandar simple **********************************************    
+    });
+  }
+  //DESVIACION ESTANDAR SIMPLE
+    if(opcion == 3){
+    const desviacionEst = {};
+    llaves.forEach(llave => {
+      if(llave != "sl_no"){      
+      desviacionEst[llave] = 0;      
+      }
+    });
     //segunda parte B: encontrar la desviacion estandar
     //sumamos las diferencias al cuadrado
     this.settings.dataset.forEach(elemento => {
@@ -151,12 +162,13 @@ export class NormalizacionPage implements OnInit {
       desviacionEst[llave] = Math.sqrt(desviacionEst[llave]);
     });
     //asignacion al dataset
-    /*this.settings.dataset.forEach(elemento => {
+    this.settings.dataset.forEach(elemento => {
       llaves.forEach(llave => {
         if (llave != "sl_no" )
         elemento[llave] = +((+elemento[llave] - media[llave]) / desviacionEst[llave] );
       });
-    });*/
+    });
+    }
     /*console.log("encontrado como media:")
     console.log(media)    
     console.log("encontrado como desviacion estandar:")
